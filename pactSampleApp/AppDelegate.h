548a09e0cf2b99dda046bbc81cf80207e9615db5
JoeMathew <joemathew@192-168-1-122.tpgi.com.au>
2015-07-25 12:30:18 +1000

    Initial Commit

---
+++ pactSampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pactSampleApp
+//
+//  Created by Joe Mathew on 25/07/2015.
+//  Copyright (c) 2015 Joe Mathew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pactSampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pactSampleApp
+//
+//  Created by Joe Mathew on 25/07/2015.
+//  Copyright (c) 2015 Joe Mathew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
